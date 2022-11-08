d95612ca66b8d1d013cd1a917dd0b2206f741d26
wpmdalamin <mdalamin7g@gmail.com>
2022-11-08 23:38:52 +0600

    dotenv configer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,10 +9,11 @@ app.use(cors())
 app.use(express.json());
 
 
-const user = "mdphotography";
-const password = "6gcPLg4U1o68YCkF";
+const user = process.env.USER_NAME;
+const password = process.env.PASSWORD;
 
 const uri = `mongodb+srv://${user}:${password}@cluster0.hdi07kd.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
